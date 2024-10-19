2ce64307476e08443dc2ddecb7dbcaaf9b4f3ffa
Daddy4AI <alanqwyuan@gmail.com>
2024-10-19 23:45:03 +0800

    initial

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,17 +1,17 @@
 // firebaseConfig.js
-import { initializeApp } from 'firebase/app';
-import { getDatabase } from 'firebase/database';
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js';
+import { getDatabase } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-database.js';
 
 // Firebase 配置
 export const firebaseConfig = {
-  apiKey: "AIzaSyD4luqKnpoQvBGB4f6BuvwUAzTXqXQrnpI",
-  authDomain: "superstarw-3ddc6.firebaseapp.com",
-  databaseURL: "https://superstarw-3ddc6-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "superstarw-3ddc6",
-  storageBucket: "superstarw-3ddc6.appspot.com",
-  messagingSenderId: "11393876466",
-  appId: "1:11393876466:web:667884c8ea24c95794ab6b",
-  measurementId: "G-HFFB83KWCP"
+    apiKey: "AIzaSyD4luqKnpoQvBGB4f6BuvwUAzTXqXQrnpI",
+    authDomain: "superstarw-3ddc6.firebaseapp.com",
+    databaseURL: "https://superstarw-3ddc6-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "superstarw-3ddc6",
+    storageBucket: "superstarw-3ddc6.appspot.com",
+    messagingSenderId: "11393876466",
+    appId: "1:11393876466:web:667884c8ea24c95794ab6b",
+    measurementId: "G-HFFB83KWCP"
 };
 
 // 初始化 Firebase
