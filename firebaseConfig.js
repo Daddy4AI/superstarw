bbe80910d0dd9ef297d787005d1d43b66db67250
Daddy4AI <alanqwyuan@gmail.com>
2024-11-02 16:49:18 +0800

    firestone

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,12 +1,10 @@
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.4.1/firebase-app.js';
-import { getDatabase } from 'https://www.gstatic.com/firebasejs/9.4.1/firebase-database.js';
-
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js';
+import { getFirestore, enableIndexedDbPersistence } from 'https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js';
 
 // Firebase 配置
 export const firebaseConfig = {
     apiKey: "AIzaSyD4luqKnpoQvBGB4f6BuvwUAzTXqXQrnpI",
     authDomain: "superstarw-3ddc6.firebaseapp.com",
-    databaseURL: "https://superstarw-3ddc6-default-rtdb.asia-southeast1.firebasedatabase.app",
     projectId: "superstarw-3ddc6",
     storageBucket: "superstarw-3ddc6.appspot.com",
     messagingSenderId: "11393876466",
@@ -14,7 +12,18 @@ export const firebaseConfig = {
     measurementId: "G-HFFB83KWCP"
 };
 
-
 // 初始化 Firebase
 const app = initializeApp(firebaseConfig);
-export const database = getDatabase(app); // Ensure this line exists
+export const db = getFirestore(app);
+
+// 启用离线持久性
+enableIndexedDbPersistence(db)
+    .catch((err) => {
+        if (err.code === 'failed-precondition') {
+            // 多个选项卡打开时，只能在一个选项卡中启用持久性
+            console.warn('Multiple tabs open, persistence can only be enabled in one tab at a time.');
+        } else if (err.code === 'unimplemented') {
+            // 当前浏览器不支持所有必要的功能
+            console.warn('The current browser does not support all of the features required to enable persistence');
+        }
+    });
